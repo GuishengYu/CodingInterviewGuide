9a0f784608d1404eee93089d7b4d91c79dbe130a
GuishengYu <940360153@qq.com>
2016-10-18 14:11:58 +0800

    all

---
+++ src/chapter4/Fibonacci.java
@@ -0,0 +1,21 @@
+package chapter4;
+
+public class Fibonacci {
+	
+	//方法一，O(2^N)
+	
+	public int f1(){
+		
+		
+		return 0;
+	}
+	//方法二，O(N)
+	//方法三，O（log N）
+	
+	
+	public static void main(String [] args){
+		
+	
+		
+	}
+}
