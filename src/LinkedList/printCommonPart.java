f080f72cb74de457f283fd415237dc6feeb37eb1
GuishengYu <940360153@qq.com>
2016-12-02 13:00:24 +0800

    builit env

---
+++ src/LinkedList/printCommonPart.java
@@ -0,0 +1,13 @@
+package LinkedList;
+/*
+ * Athor : Yuze Yu
+ * Date : 2016.12.01
+ * */
+public class printCommonPart {
+	
+	public  static void  main(String [] args){
+		System.out.println("Hello tomorrow!!");
+	}
+}
+class Node{
+}
